0e4283bfb65d1d9b8af7fcc2afa2b1098281bfc2
Piero Coronado <pacoronado01@gmail.com>
2024-01-23 18:49:32 -0500

    Finished Swift UI Essentials Module

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by Piero Coronado on 1/23/24.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack(alignment: .leading) {
+                    Text("Seconds Elapsed")
+                        .font(.caption)
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                Spacer()
+                VStack(alignment: .trailing) {
+                    Text("Seconds Remaining")
+                        .font(.caption)
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+            .accessibilityElement(children: .ignore)
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            HStack {
+                Text("Speaker 1 of 3")
+                Spacer()
+                Button(action: {}) {
+                    Image(systemName: "forward.fill")
+                }
+            }
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    MeetingView()
+}
